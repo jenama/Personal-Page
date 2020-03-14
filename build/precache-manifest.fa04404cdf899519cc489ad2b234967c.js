758080cf12a500b9d690b5b7cf8922efc64b3c6f
jenama <johanneenama@gmail.com>
2020-03-14 18:51:29 -0400

    Add build

---
+++ build/precache-manifest.fa04404cdf899519cc489ad2b234967c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609d0d8d4dc03438b78b29778993adb4",
+    "url": "/jenama/Personal-Page/index.html"
+  },
+  {
+    "revision": "93556b8db08f9794ac32",
+    "url": "/jenama/Personal-Page/static/css/main.541db6a9.chunk.css"
+  },
+  {
+    "revision": "a4df5e512d6c7a31154b",
+    "url": "/jenama/Personal-Page/static/js/2.58f110bb.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/jenama/Personal-Page/static/js/2.58f110bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93556b8db08f9794ac32",
+    "url": "/jenama/Personal-Page/static/js/main.c0c04510.chunk.js"
+  },
+  {
+    "revision": "70d346dd3f8c80727db1",
+    "url": "/jenama/Personal-Page/static/js/runtime-main.85712ef3.js"
+  }
+]);
